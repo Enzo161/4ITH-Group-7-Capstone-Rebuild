d7cfe012adebd9d4a41dbac97a5921f97c2e6525
Lorenzo V. Plantilla <lorenzo.plantilla.cics@ust.edu.ph>
2022-10-30 14:28:03 +0800

    Back-end tweaks

--- db/migrate/20221029113704_add_devise_two_factor_to_users.rb
+++ db/migrate/20221029113704_add_devise_two_factor_to_users.rb
@@ -1,5 +1,10 @@
 class AddDeviseTwoFactorToUsers < ActiveRecord::Migration[6.0]
   def change
+    remove_column :users, :encrypted_otp_secret
+    remove_column :users, :encrypted_otp_secret_iv
+    remove_column :users, :encrypted_otp_secret_salt
+    remove_column :users, :consumed_timestep
+    remove_column :users, :otp_required_for_login
     add_column :users, :encrypted_otp_secret, :string
     add_column :users, :encrypted_otp_secret_iv, :string
     add_column :users, :encrypted_otp_secret_salt, :string
