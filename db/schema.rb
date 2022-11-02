9962fe8bd4810ab5daba26035e20b051a30def07
Sophia Loste <111494593+sophialosteust@users.noreply.github.com>
2022-11-02 15:32:51 +0800

    Minor fixes

--- db/schema.rb
+++ db/schema.rb
@@ -16,32 +16,32 @@ ActiveRecord::Schema.define(version: 2022_11_01_160740) do
   enable_extension "plpgsql"
 
   create_table "active_storage_attachments", force: :cascade do |t|
-    t.string "name", null: false
+    t.text "name", null: false
     t.string "record_type", null: false
-    t.integer "record_id", null: false
-    t.integer "blob_id", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
     t.datetime "created_at", null: false
     t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
     t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
   end
 
   create_table "active_storage_blobs", force: :cascade do |t|
-    t.string "key", null: false
-    t.string "filename", null: false
-    t.string "content_type"
+    t.text "key", null: false
+    t.text "filename", null: false
+    t.text "content_type"
     t.text "metadata"
     t.bigint "byte_size", null: false
-    t.string "checksum", null: false
+    t.text "checksum", null: false
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
-  create_table "activities", force: :cascade do |t|
+  create_table "activities", id: :serial, force: :cascade do |t|
     t.string "trackable_type"
     t.integer "trackable_id"
     t.string "owner_type"
     t.integer "owner_id"
-    t.string "key"
+    t.text "key"
     t.text "parameters"
     t.string "recipient_type"
     t.integer "recipient_id"
@@ -64,74 +64,65 @@ ActiveRecord::Schema.define(version: 2022_11_01_160740) do
   end
 
   create_table "clients", force: :cascade do |t|
-    t.string "client_name"
-    t.integer "island_id", null: false
+    t.text "client_name"
+    t.bigint "island_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["island_id"], name: "index_clients_on_island_id"
   end
 
   create_table "deep_wells", force: :cascade do |t|
-    t.string "deep_well_name"
-    t.integer "client_id", null: false
+    t.text "deep_well_name"
+    t.bigint "client_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "island_id"
     t.date "dateGranted"
-    t.string "clientName"
-    t.string "wellLocation"
-    t.string "wellCoodinates"
-    t.string "waterPermitNo"
-    t.string "contractor"
-    t.string "depthTotal"
-    t.date "dateofUTVI"
-    t.text "reason"
+    t.text "clientName"
+    t.text "wellLocation"
+    t.text "wellCoodinates"
+    t.text "waterPermitNo"
+    t.text "depthTotal"
     t.date "dateConstructed"
     t.integer "limit"
     t.text "driller_name"
     t.text "remarks"
     t.index ["client_id"], name: "index_deep_wells_on_client_id"
-    t.index ["island_id"], name: "index_deep_wells_on_island_id"
   end
 
   create_table "islands", force: :cascade do |t|
-    t.string "island_name"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "tests", force: :cascade do |t|
+    t.text "island_name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
+    t.text "email", default: "", null: false
+    t.text "encrypted_password", default: "", null: false
+    t.text "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.integer "sign_in_count", default: 0, null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string "current_sign_in_ip"
-    t.string "last_sign_in_ip"
+    t.text "current_sign_in_ip"
+    t.text "last_sign_in_ip"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.boolean "admin", default: false
-    t.string "first_name"
-    t.string "last_name"
-    t.string "invitation_token"
+    t.text "first_name"
+    t.text "last_name"
+    t.text "username"
+    t.text "invitation_token"
     t.datetime "invitation_created_at"
     t.datetime "invitation_sent_at"
     t.datetime "invitation_accepted_at"
     t.integer "invitation_limit"
     t.string "invited_by_type"
-    t.integer "invited_by_id"
+    t.bigint "invited_by_id"
     t.integer "invitations_count", default: 0
-    t.string "encrypted_otp_secret"
-    t.string "encrypted_otp_secret_iv"
-    t.string "encrypted_otp_secret_salt"
+    t.text "encrypted_otp_secret"
+    t.text "encrypted_otp_secret_iv"
+    t.text "encrypted_otp_secret_salt"
     t.integer "consumed_timestep"
     t.boolean "otp_required_for_login"
     t.index ["email"], name: "index_users_on_email", unique: true
@@ -139,20 +130,10 @@ ActiveRecord::Schema.define(version: 2022_11_01_160740) do
     t.index ["invited_by_id"], name: "index_users_on_invited_by_id"
     t.index ["invited_by_type", "invited_by_id"], name: "index_users_on_invited_by_type_and_invited_by_id"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-  end
-
-  create_table "versions", force: :cascade do |t|
-    t.string "item_type", null: false
-    t.bigint "item_id", null: false
-    t.string "event", null: false
-    t.string "whodunnit"
-    t.text "object"
-    t.datetime "created_at"
-    t.index ["item_type", "item_id"], name: "index_versions_on_item_type_and_item_id"
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "clients", "islands"
   add_foreign_key "deep_wells", "clients"
-  add_foreign_key "deep_wells", "islands"
 end
