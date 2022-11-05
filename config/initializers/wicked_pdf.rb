e1d243dec1ffe9446e9bfe8cba1a47ce3139b2b3
Sophia Loste <111494593+sophialosteust@users.noreply.github.com>
2022-11-06 00:41:48 +0800

    Updated wicked_pdf.rb

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -1,4 +1,7 @@
 WickedPdf.config = {
-  exe_path: 'C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe',
+  #exe_path: 'C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe', 
+  exe_path: '#{Rails.root}/bin/wkhtmltopdf',
   enable_local_file_access: true
+    
+
 }
