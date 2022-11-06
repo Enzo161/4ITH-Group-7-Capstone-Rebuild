99f483c61cec0a18a6e9c7a6d19a4b88fbbaaa90
Lorenzo V. Plantilla <lorenzo.plantilla.cics@ust.edu.ph>
2022-11-06 15:54:42 +0800

    Update wicked_pdf.rb

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -4,7 +4,7 @@
   #enable_local_file_access: true
 
   if Rails.env.staging? || Rails.env.production?
-  WickedPdf.config = {:exe_path => Rails.root.join('bin', 'wkhtmltopdf-64-bit').to_s}
+  WickedPdf.config = {:exe_path => Rails.root.join('bin', 'wkhtmltopdf-64bit').to_s}
   else
   WickedPdf.config = { :exe_path => 'C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe'}
 end
